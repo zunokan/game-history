023e69c99fcf4d9c27a16b84a7a322aea2919961
Zuno <zunokan@gmail.com>
2018-03-13 15:01:16 -0300

    Added basic user login and template

--- GameHistory/GameHistory/settings.py
+++ GameHistory/GameHistory/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -78,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gamehistory',
         'USER': 'zuno',
-        'PASSWORD': '',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '',
     }
